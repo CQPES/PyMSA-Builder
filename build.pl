1121fdd391ff1d76c4cd3bcd0d29abc197851619
mizu-bai <shiragawa4519@outlook.com>
2023-02-06 09:37:30 +0800

    Update

--- build.pl
+++ build.pl
@@ -31,15 +31,18 @@ $in = <STDIN>;
 chomp $in;
 die "Goodbye!\n" unless $in =~ m/y/i or not $in;
 
+# check `f2py`
 $which_f2py = `which f2py`;
 chomp $which_f2py;
 
 die "`Error: f2py` not found, building failed!\n" unless $which_f2py;
 
+# cleanup?
 print "Clean up after building? [Y/n] ";
 $in = <STDIN>;
 chomp $in;
 
+# parameters for PIP
 print "max degree of PIP: ";
 $degree = <STDIN>;
 chomp $degree;
@@ -52,21 +55,25 @@ print "range parameter for Morse-like variables (default 2.0d0): ";
 $alpha = <STDIN>;
 chomp $alpha;
 
+# basis
 say "Generating PIP basis...";
 `./msa $degree $molecule`;
 
+# Fortran codes
 say "Generating Fortran files...";
 `perl postemsa.pl $degree $molecule`;
 `perl derivative.pl $degree $molecule`;
-`sed -i 's/real/real*8/g' *.f90`;
-`sed -i 's/a = 2.0d0/a = $alpha/g' gradient.f90` if $alpha;
+`perl -pi -e "s/real/real\*8/g" *.f90`;
+`perl -pi -e "s/a\s+=\s+2.0d0/a = $alpha/g" gradient.f90` if $alpha;
 
+# Python module
 say "Building Python module `msa`...";
 `f2py basis.f90 gradient.f90 -m msa -h msa.pyf --overwrite-signature` or die "Building Error!\n";
 `f2py -c basis.f90 gradient.f90 msa.pyf` or die "Building Error!\n";
 say "Done!";
 say "To test Python module `msa`, run command:";
-say "\$ python -c \"from msa import basis, gradient; print(basis.__doc__); print(gradient.__doc__)\"";
+say "\$ python3 -c \"from msa import basis, gradient; print(basis.__doc__); print(gradient.__doc__)\"";
 say "Please check the output if any error exists!";
 
+# cleanup
 `rm -rf *f90 *pyf MOL*` if $in =~ m/y/i or not $in;
